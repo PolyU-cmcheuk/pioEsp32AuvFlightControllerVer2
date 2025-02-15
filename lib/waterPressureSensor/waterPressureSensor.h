92bfdf4351cd5566a5c19de42c365ea59dc0f860
auv pi ver2 ming-build <fortegspex@gmail.com>
2025-02-15 18:23:33 +0800

    start working on bar30 water pressure sensor

---
+++ lib/waterPressureSensor/waterPressureSensor.h
@@ -0,0 +1,11 @@
+#ifndef WATERPRESSURESENSOR_H
+#define WATERPRESSURESENSOR_H
+
+// need to include arduino library for function: Delay(),
+#include <Arduino.h>
+// Bar30 sensor needs I2C
+#include <Wire.h>
+// bar30 sensor contributed lib
+#include "MS5837.h"
+
+#endif // WATERPRESSURESENSOR_H
